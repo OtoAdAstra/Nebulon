9e2469630512f0a2390276b5aa98b6def0bc957e
Oto <sharvashidze0712@gmail.com>
2026-03-01 21:26:42 +0400

    first commit of greatness

---
+++ Nebulon/NebulonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NebulonApp.swift
+//  Nebulon
+//
+//  Created by Oto Sharvashidze on 01.03.26.
+//
+
+import SwiftUI
+
+@main
+struct NebulonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
